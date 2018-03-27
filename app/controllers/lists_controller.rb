5fc93458b62be15b89117297af4cc7ba70495575
ダルビッシュ <shoei0205@gmail.com>
2018-03-27 11:48:01 +0900

    セーブ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,12 +10,23 @@ class ListsController < ApplicationController
   end
 
   def create
-
+    # パラメーター(リストの名前)と共にリスト作成
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list), notice: "リスティングを作成・保存をしました"
+    else
+      redirect_to new_list_path, notice: "リスティングを作成・保存出来ませんでした"
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name, :status)
   end
 end
