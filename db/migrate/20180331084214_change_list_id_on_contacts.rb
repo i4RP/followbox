f6971f2e4201308d450e953ed39dd393435eccc1
ダルビッシュ <shoei0205@gmail.com>
2018-04-09 19:22:27 +0900

    ああ

--- db/migrate/20180331084214_change_list_id_on_contacts.rb
+++ db/migrate/20180331084214_change_list_id_on_contacts.rb
@@ -1,7 +1,7 @@
 class ChangeListIdOnContacts < ActiveRecord::Migration[5.1]
   def change
     change_table :contacts do |t|
-  t.change :list_id, :string
+    t.change :list_id, :integer
     end
   end
 end
